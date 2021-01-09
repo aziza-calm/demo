030623cb1e0c00d8c8623465088bee4829b020c1
aziza.kalmasova <aziza.kalmasova@phystech.edu>
2021-01-09 23:15:22 +0300

    Initial commit

---
+++ src/main/java/aidashop/demo/HorrorShopApplication.java
@@ -0,0 +1,13 @@
+package aidashop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorrorShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorrorShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aidashop/demo/HorrorShopApplicationTests.java
@@ -0,0 +1,13 @@
+package aidashop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorrorShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
